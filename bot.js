48541e0a7a715dcf504aebfd209fffdb1cc11f69
dorron <dorron@pop-os.localdomain>
2022-11-22 10:24:19 -0500

    so cron dont fail

--- bot.js
+++ bot.js
@@ -19,7 +19,7 @@ const client = new Client({
 client.once("ready", () => {
     client.channels.cache.forEach(async c => {
         const channelId = c.id
-        if(c.guild.me.permissionsIn(c.id).has('MANAGE_WEBHOOKS') && c.guild.me.permissionsIn(c.id).has('SEND_MESSAGES')) {
+        if(c.guild.me.permissionsIn(c.id).has('MANAGE_WEBHOOKS') && c.guild.me.permissionsIn(c.id).has('SEND_MESSAGES') && c.name.includes('playground')) {
             console.log(c.name)
 
             const data  = await fetchData()
@@ -48,7 +48,7 @@ client.once("ready", () => {
 })
 
 
-app.get('/fetch-and-share', async (req, res) => {
+app.get('/', async (req, res) => {
     res.end('ran')
 })
 
